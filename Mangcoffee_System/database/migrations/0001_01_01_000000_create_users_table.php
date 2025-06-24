7866ee7b17124b97e2ae2095bccb23f6c4477e5b
home_joiling_ <home_joilingford@gmail.com>
2025-06-24 16:22:35 +0800

    ok

--- Mangcoffee_System/database/migrations/0001_01_01_000000_create_users_table.php
+++ Mangcoffee_System/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,18 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-
-            $table->string('username')->unique();
-            $table->string('firstname');
-            $table->string('middlename')->nullable();
-            $table->string('lastname');
-            $table->string('address');
-            $table->string('email')->unique(); // Required for login/email verification
-            $table->timestamp('email_verified_at')->nullable(); // Email verification timestamp
-            $table->string('password'); // Required for authentication
-            $table->string('role')->default('admin', 'user'); // 'admin', 'user', etc.
-            $table->string('image')->nullable(); // path to uploaded profile image
-
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -50,8 +42,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
--- Mangcoffee_System/database/seeders/DatabaseSeeder.php
+++ Mangcoffee_System/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Joilingford',
-            'email' => 'Joilingford@gmail.com',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
